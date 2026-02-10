e59298d3fd7310b80dcedc833b69b78d95bbb5e7
Derek Laventure <sensespidey@gmail.com>
2026-02-10 11:53:36 -0500

    Adjust startDev.sh to call hypnotoad directly with PERL5LIB set to include local/lib.

--- startDev.sh
+++ startDev.sh
@@ -1,2 +1,3 @@
 #/bin/bash
-MOJO_MODE=development carton exec hypnotoad app.pl
+MOJO_MODE=development PERL5LIB=.:local/lib/perl5 perl ./local/bin/hypnotoad app.pl
+#MOJO_MODE=development carton exec hypnotoad app.pl
